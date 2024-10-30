3315f8f20ebe8cb233d1f2b32aab732f3523ea3c
Teyfik Anıl TEKDEMİR <taniltekdemir@gmail.com>
2024-10-30 22:24:34 +0300

    enhancement completed about endpoint of customer-service

--- db/db.go
+++ db/db.go
@@ -1,9 +1,52 @@
 package db
 
-func GetDB(secrets map[string]string) {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+)
+
+type PostgresDB struct {
+	db *sqlx.DB
 }
 
-func createDB(secrets map[string]string) {
+func GetDB(secrets map[string]string) *PostgresDB {
+	db := createDB(secrets)
+	return &PostgresDB{
+		db: db,
+	}
+}
 
+func createDB(secrets map[string]string) *sqlx.DB {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return nil
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s",
+		dbHost, dbPort, secrets["username"], secrets["password"])
+
+	db, err := sqlx.Connect("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Create customers table
+	createTableSQL := `
+	CREATE TABLE customers (
+	    id SERIAL PRIMARY KEY,
+	    name VARCHAR(255),
+	    email VARCHAR(255) UNIQUE,
+	    address VARCHAR(255)
+	);
+	`
+	_, err = db.Exec(createTableSQL)
+
+	return db
 }
--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"customer-service/db"
+	"customer-service/service"
 )
 
 func main() {
+
+	secret := db.GetSecretValue()
+	db := db.GetDB(secret)
+	a := service.GetApp(db)
+
 	r := gin.Default()
 
-	r.POST("/cutomers", func(c *gin.Context) {})
-	r.GET("/customers/:customerId", func(c *gin.Context) {})
-	r.PUT("/customers/:customerId", func(c *gin.Context) {})
-	r.DELETE("/customers/:customerId", func(c *gin.Context) {})
+	r.POST("/cutomers", a.PostHandler)
+	r.GET("/customers/:customerId", a.GetHandler)
+	r.PUT("/customers/:customerId", a.PutHandler)
+	r.DELETE("/customers/:customerId", a.DeleteHandler)
 
 	r.Run("localhost:8080")
 }
